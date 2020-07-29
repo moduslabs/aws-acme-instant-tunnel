3291de348d478c619cdcdf1abfcc0fb0df8958af
Tim Vu <timmyvu2020@gmail.com>
2020-07-29 11:16:16 -0400

    added function to authorize SG Ingress

--- handler.js
+++ handler.js
@@ -3,13 +3,8 @@ const AWS = require('aws-sdk');
 const { uuid } = require('uuidv4');
 const helper = require('./helper');
 const TABLE_NAME = process.env.DYNAMODB_TABLE;
+const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
 let dynamo = new AWS.DynamoDB.DocumentClient();
-function createResponse(statusCode, message) {
-  return {
-    statusCode: statusCode,
-    body: JSON.stringify(message),
-  };
-}
 module.exports.addLease = (event, context, callback) => {
   const item = JSON.parse(JSON.stringify(event));
   console.log(item);
@@ -24,6 +19,7 @@ module.exports.addLease = (event, context, callback) => {
     .then(() => {
       callback(null, createResponse(200, item.leaseId));
     });
+  addNewPermissions(event, context, callback);
 };
 module.exports.updateExpiredLeases = async (event, context, callback) => {
   const items = await helper.getExpiredLeases();
@@ -48,8 +44,40 @@ module.exports.updateExpiredLeases = async (event, context, callback) => {
         if (err) {
           callback(err, null);
         } else {
+          console.log(response);
           callback(null, createResponse(200, response));
         }
       });
   });
 };
+function createResponse(statusCode, message) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(message),
+  };
+}
+async function addNewPermissions(event, context, callback) {
+  const id = await helper.getSecurityGroupId();
+  const sgParams = {
+    GroupId: id,
+    IpPermissions: [
+      {
+        FromPort: 22,
+        IpProtocol: 'tcp',
+        IpRanges: [
+          {
+            CidrIp: `${event.ip}/24`,
+            Description: `Access for ${event.name}`,
+          },
+        ],
+        ToPort: 22,
+      },
+    ],
+  };
+  ec2
+    .authorizeSecurityGroupIngress(sgParams)
+    .promise()
+    .then((result) => {
+      callback(null, createResponse(200, result));
+    });
+}
--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 'use strict';
 const AWS = require('aws-sdk');
 let dynamo = new AWS.DynamoDB.DocumentClient();
-
+const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
 const TABLE_NAME = process.env.DYNAMODB_TABLE;
 const currentTime = Date.now();
 module.exports.getExpiredLeases = () => {
@@ -21,5 +21,17 @@ module.exports.getExpiredLeases = () => {
     .promise()
     .then((result) => {
       return result.Items;
+    });
+};
+module.exports.getSecurityGroupId = () => {
+  var params = {
+    DryRun: false,
+  };
+  // Call EC2 to retrieve policy for selected bucket
+  return ec2
+    .describeInstances(params)
+    .promise()
+    .then((result) => {
+      return result.Reservations[0].Instances[0].SecurityGroups[0].GroupId;
     });
 };
