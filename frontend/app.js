35e1292bcee77a8492cd0c8e39234dd844391eb0
Tim Vu <timmyvu2020@gmail.com>
2020-08-03 13:42:19 -0400

    Auth0 Authentication integrated

---
+++ frontend/app.js
@@ -0,0 +1,134 @@
+/* global window document localStorage fetch alert */
+
+// Fill in with your values
+const AUTH0_CLIENT_ID = 'srZfVKQ01KiWqdR82gIX8kdNf5dc0goU';
+const AUTH0_DOMAIN = 'acme-instant-tunnel.us.auth0.com';
+const AUTH0_CALLBACK_URL = window.location.href; // eslint-disable-line
+//const PUBLIC_ENDPOINT = 'https://your-aws-endpoint-here.amazonaws.com/dev/api/public';
+const PRIVATE_ENDPOINT =
+  'https://3sjh80uyof.execute-api.us-east-1.amazonaws.com/dev/api/addLease';
+const currentTime = Date.now();
+// initialize auth0 lock
+const lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+  // eslint-disable-line no-undef
+
+  auth: {
+    params: {
+      scope: 'openid email',
+    },
+    responseType: 'token id_token',
+  },
+});
+
+function updateUI() {
+  const isLoggedIn = localStorage.getItem('id_token');
+  if (isLoggedIn) {
+    // swap buttons
+    document.getElementById('btn-login').style.display = 'none';
+    document.getElementById('btn-logout').style.display = 'inline';
+    const profile = JSON.parse(localStorage.getItem('profile'));
+    console.log(profile);
+    // show username
+    document.getElementById('nick').textContent = profile.email;
+  }
+}
+
+// Handle login
+lock.on('authenticated', (authResult) => {
+  console.log(authResult);
+  lock.getUserInfo(authResult.accessToken, (error, profile) => {
+    if (error) {
+      // Handle error
+      return;
+    }
+
+    document.getElementById('nick').textContent = profile.nickname;
+
+    localStorage.setItem('accessToken', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('profile', JSON.stringify(profile));
+
+    updateUI();
+  });
+});
+
+updateUI();
+
+// Handle login
+document.getElementById('btn-login').addEventListener('click', () => {
+  lock.show();
+});
+
+// Handle logout
+document.getElementById('btn-logout').addEventListener('click', () => {
+  localStorage.removeItem('id_token');
+  localStorage.removeItem('access_token');
+  localStorage.removeItem('profile');
+  document.getElementById('btn-login').style.display = 'flex';
+  document.getElementById('btn-logout').style.display = 'none';
+  document.getElementById('nick').textContent = '';
+});
+
+// Handle public api call
+// document.getElementById('btn-public').addEventListener('click', () => {
+//   // call public API
+//   fetch(PUBLIC_ENDPOINT, {
+//     cache: 'no-store',
+//     method: 'POST',
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       console.log('Message:', data);
+//       document.getElementById('message').textContent = '';
+//       document.getElementById('message').textContent = data.message;
+//     })
+//     .catch((e) => {
+//       console.log('error', e);
+//     });
+// });
+
+// Handle private api call
+document.getElementById('btn-private').addEventListener('click', async () => {
+  console.log('here');
+  // Call private API with JWT in header
+  const profile = JSON.parse(localStorage.getItem('profile'));
+  const token = localStorage.getItem('id_token');
+  // block request from happening if no JWT token present
+  if (!token) {
+    document.getElementById('message').textContent = '';
+    document.getElementById('message').textContent =
+      'You must login to call this protected endpoint!';
+    return false;
+  }
+  const ipAddress = await fetch('https://api.ipify.org?format=json')
+    .then((result) => result.json())
+    .then((data) => {
+      return data.ip;
+    });
+  console.log(ipAddress);
+  // Do request to private endpoint
+  fetch(PRIVATE_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      leaseActive: true,
+      leaseEnd: currentTime + 3600,
+      leaseStart: currentTime,
+      name: profile.email,
+      ip: `${ipAddress}/32`,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Token:', data);
+      document.getElementById('message').textContent = '';
+      document.getElementById('message').textContent = data.message;
+    })
+    .catch((e) => {
+      console.log('error', e);
+    });
+});
--- handler.js
+++ handler.js
@@ -1,14 +1,91 @@
 'use strict';
 const AWS = require('aws-sdk');
-const { uuid } = require('uuidv4');
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
 const helper = require('./helper');
+
 const TABLE_NAME = process.env.DYNAMODB_TABLE;
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const AUTH0_CLIENT_PUBLIC_KEY = process.env.AUTH0_CLIENT_PUBLIC_KEY;
 const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+const currentTime = Date.now();
 let dynamo = new AWS.DynamoDB.DocumentClient();
+
+const generatePolicy = (principalId, effect, resource) => {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+module.exports.auth = (event, context, callback) => {
+  console.log('event', event);
+  if (!event.authorizationToken) {
+    return callback('Unauthorized');
+  }
+
+  const tokenParts = event.authorizationToken.split(' ');
+  const tokenValue = tokenParts[1];
+
+  if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
+    // no auth token!
+    return callback('Unauthorized');
+  }
+  const options = {
+    audience: AUTH0_CLIENT_ID,
+  };
+
+  try {
+    jwt.verify(
+      tokenValue,
+      AUTH0_CLIENT_PUBLIC_KEY,
+      options,
+      (verifyError, decoded) => {
+        if (verifyError) {
+          console.log('verifyError', verifyError);
+          // 401 Unauthorized
+          console.log(`Token invalid. ${verifyError}`);
+          return callback('Unauthorized');
+        }
+        // is custom authorizer function
+        console.log('valid from customAuthorizer', decoded);
+        return callback(
+          null,
+          generatePolicy(decoded.sub, 'Allow', event.methodArn)
+        );
+      }
+    );
+  } catch (err) {
+    console.log('catch error. Invalid token', err);
+    return callback('Unauthorized');
+  }
+};
 module.exports.addLease = (event, context, callback) => {
-  const item = JSON.parse(JSON.stringify(event));
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      message: 'You can now SSH into the EC2 instance for 1 hour',
+    }),
+  });
+  let item = JSON.parse(JSON.stringify(event));
+  item = JSON.parse(item.body);
+  item.leaseId = uuidv4();
   console.log(item);
-  item.leaseId = uuid();
   const params = {
     TableName: TABLE_NAME,
     Item: item,
@@ -22,7 +99,7 @@ module.exports.addLease = (event, context, callback) => {
     .catch(() => {
       callback(err, null);
     });
-  addNewPermissions(event, context, callback);
+  addNewPermissions(item, context, callback);
 };
 module.exports.updateExpiredLeases = async (event, context, callback) => {
   const items = await helper.getExpiredLeases();
@@ -56,10 +133,16 @@ module.exports.updateExpiredLeases = async (event, context, callback) => {
 function createResponse(statusCode, message) {
   return {
     statusCode: statusCode,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify(message),
   };
 }
-async function addNewPermissions(event, context, callback) {
+async function addNewPermissions(item, context, callback) {
   const id = await helper.getSecurityGroupId();
   const sgParams = {
     GroupId: id,
@@ -69,8 +152,8 @@ async function addNewPermissions(event, context, callback) {
         IpProtocol: 'tcp',
         IpRanges: [
           {
-            CidrIp: event.ip,
-            Description: `Access for ${event.name}`,
+            CidrIp: item.ip,
+            Description: `Access for ${item.name}`,
           },
         ],
         ToPort: 22,
